8189b4680091b537edeeea4975d426cf1aee602f
Ayelangbe Samuel <ayelangbeadeyinka@gmail.com>
2025-10-24 09:43:41 +0100

    web page correction

--- Abfet/Abj/abfet.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('contact-form').addEventListener('submit', e => {
-    e.preventDefault();
-    alert('Thanks! We’ll be in touch shortly.');
-    e.target.reset();
-  });
-});
